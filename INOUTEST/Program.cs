460966921faba21067a2754d4eb720c4c30ec67b
AbexCorp <bartek.fryczek@onet.pl>
2024-03-03 20:34:46 +0100

    INOUTEST INTEST

---
+++ INOUTEST/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace INOUTEST
+{
+    class Program
+    {
+        public static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                sb.AppendLine((numbers[0] * numbers[1]).ToString());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ INTEST/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace INTEST
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] firstInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int testCases = int.Parse(firstInput[0]), mod = int.Parse(firstInput[1]);
+            int counter = 0;
+            for (int i = 0; i < testCases; i++)
+            {
+                counter = int.Parse(Console.ReadLine()) % mod == 0 ? counter + 1 : counter;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
