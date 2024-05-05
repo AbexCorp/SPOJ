c8405d8bab22b99069f6e068417b3ddc98b33354
AbexCorp <bartek.fryczek@onet.pl>
2024-05-05 17:07:45 +0200

    STPAR

---
+++ STPAR/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace STPAR
+{
+    class Program
+    {
+        public static StringBuilder sb = new StringBuilder();
+
+        public static Queue<int> exit = new Queue<int>();
+        public static Stack<int> sideStreet = new Stack<int>();
+        static int min;
+
+
+        public static void Main()
+        {
+            int carNumber;
+            while((carNumber = int.Parse(Console.ReadLine())) != 0)
+            {
+                int[] cars = Array.ConvertAll(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                min = 0;
+                for (int i = 0; i  < cars.Length; i++)
+                {
+                    while(sideStreet.Count != 0)
+                    {
+                        if (sideStreet.Peek() == min + 1)
+                        {
+                            exit.Enqueue(sideStreet.Pop());
+                            min++;
+                        }
+                        else
+                            break;
+                    }
+                    if (cars[i] == min + 1)
+                    {
+                        exit.Enqueue(cars[i]);
+                        min++;
+                    }
+                    else
+                    {
+                        sideStreet.Push(cars[i]);
+                    }
+                }
+                while(sideStreet.Count != 0)
+                {
+                    if (sideStreet.Peek() == min + 1)
+                    {
+                        exit.Enqueue(sideStreet.Pop());
+                        min++;
+                    }
+                    else
+                        break;
+                }
+
+                if (sideStreet.Count == 0)
+                    sb.AppendLine("yes");
+                else
+                    sb.AppendLine("no");
+
+                exit.Clear();
+                sideStreet.Clear();
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
